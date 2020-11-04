16f87e3046643b07bd8b3a4acf159079497dfa38
summersmooth <winterhard1234@gmail.com>
2020-11-04 12:48:34 +0900

    fix: Display 3 decimal places on token balance

--- src/utils/formatBalance.ts
+++ src/utils/formatBalance.ts
@@ -1,10 +1,10 @@
 import { BigNumber } from 'ethers';
 
 export const getDisplayBalance = (balance: BigNumber, decimals = 18) => {
-  const displayBalance = balance.div(BigNumber.from(10).pow(decimals))
-  return displayBalance.toString()
+  const number = balance.div(BigNumber.from(10).pow(decimals - 3)).toNumber();
+  return (number / 10 ** 3).toFixed(3);
 }
 
 export const getFullDisplayBalance = (balance: BigNumber, decimals = 18) => {
-  return balance.div(BigNumber.from(10).pow(decimals)).toString();
+  return getDisplayBalance(balance, decimals);
 }
