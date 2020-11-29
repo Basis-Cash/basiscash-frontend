56888047a188c84421de76250f559ef0c58147fd
summersmooth <winterhard1234@gmail.com>
2020-11-29 21:14:26 +0900

    feat: Update contract deployment

--- src/config.ts
+++ src/config.ts
@@ -9,46 +9,32 @@ const configurations: { [env: string]: Configuration } = {
     defaultProvider: 'wss://ropsten.infura.io/ws/v3/d3bcb3c54e8c49a7bd1a9b6773e65bbe',
     deployments: require('./basis-cash/deployments/deployments.ropsten.json'),
     externalTokens: {
-      DAI: ['0xf59b931a9e471c57306Ee330bE031B3fF28D6825', 18],
-      yCRV: ['0xf59b931a9e471c57306Ee330bE031B3fF28D6825', 18],
-      SUSD: ['0xf59b931a9e471c57306Ee330bE031B3fF28D6825', 18],
-      USDC: ['0xf59b931a9e471c57306Ee330bE031B3fF28D6825', 18],
-      USDT: ['0xf59b931a9e471c57306Ee330bE031B3fF28D6825', 18],
-      // TODO: replace with real address
-      'BAC_DAI-UNI-LPv2': ['0xEB249F8cbD61f5B6E55cf6a1A1e944aad5c25e15', 18],
-      'BAS_DAI-UNI-LPv2': ['0x849e93C8A25AaBf440864682E97B0286713a8f5d', 18],
+      DAI: ['0xd4A2e76D12f8AAb562f8072Ce029add7f25239d5', 18],
+      yCRV: ['0xd4A2e76D12f8AAb562f8072Ce029add7f25239d5', 18],
+      SUSD: ['0xd4A2e76D12f8AAb562f8072Ce029add7f25239d5', 18],
+      USDC: ['0xd4A2e76D12f8AAb562f8072Ce029add7f25239d5', 18],
+      USDT: ['0xd4A2e76D12f8AAb562f8072Ce029add7f25239d5', 18],
+      'BAC_DAI-UNI-LPv2': ['0xA78Bf231059bCAfc8Ca37164FC71a0D2C39DFf88', 18],
+      'BAS_DAI-UNI-LPv2': ['0x1e794b54b5EC6Ff44A4070469Db54efB870FC248', 18],
     },
     baseLaunchDate: new Date('2020-11-26T00:00:00Z'),
     bondLaunchesAt: new Date('2020-12-04T00:00:00Z'),
     boardroomLaunchesAt: new Date('2020-12-04T00:00:00Z'),
   },
   production: {
-    /*chainId: ChainId.MAINNET,
+    chainId: ChainId.MAINNET,
     etherscanUrl: 'https://etherscan.io',
     defaultProvider: 'wss://mainnet.infura.io/ws/v3/d3bcb3c54e8c49a7bd1a9b6773e65bbe',
-    deployments: require('./basis-cash/deployments/deployments.local.json'),
+    // TODO: replace with real mainnet deployment
+    deployments: require('./basis-cash/deployments/deployments.ropsten.json'),
     externalTokens: {
       DAI: ['0x6B175474E89094C44Da98b954EedeAC495271d0F', 18],
       YFI: ['0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e', 18],
       SUSD: ['0x57Ab1E02fEE23774580C119740129eAC7081e9D3', 18],
       USDC: ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6],
       USDT: ['0xdAC17F958D2ee523a2206206994597C13D831ec7', 6],
-      // TODO: replace with real address
       'BAC_DAI-UNI-LPv2': ['0x6B175474E89094C44Da98b954EedeAC495271d0F', 18],
-      'BAS_DAI-UNI-LPv2': ['0x6B175474E89094C44Da98b954EedeAC495271d0F', 18], */
-    chainId: ChainId.ROPSTEN,
-    etherscanUrl: 'https://ropsten.etherscan.io',
-    defaultProvider: 'wss://ropsten.infura.io/ws/v3/d3bcb3c54e8c49a7bd1a9b6773e65bbe',
-    deployments: require('./basis-cash/deployments/deployments.ropsten.json'),
-    externalTokens: {
-      DAI: ['0xbda895A8a258c5EA7A21ED6dE405384B9250Fc28', 18],
-      yCRV: ['0xbda895A8a258c5EA7A21ED6dE405384B9250Fc28', 18],
-      SUSD: ['0xbda895A8a258c5EA7A21ED6dE405384B9250Fc28', 18],
-      USDC: ['0xbda895A8a258c5EA7A21ED6dE405384B9250Fc28', 6],
-      USDT: ['0xbda895A8a258c5EA7A21ED6dE405384B9250Fc28', 6],
-      // TODO: replace with real address
-      'BAC_DAI-UNI-LPv2': ['0x9c7471738B65B2c67ca63F398D037b780fEf74ec', 18],
-      'BAS_DAI-UNI-LPv2': ['0x752125de1C8105aDCfb6BAbE0e1dB3160A7BefE4', 18],
+      'BAS_DAI-UNI-LPv2': ['0x6B175474E89094C44Da98b954EedeAC495271d0F', 18],
     },
     baseLaunchDate: new Date('2020-11-30T00:00:00Z'),
     bondLaunchesAt: new Date('2020-12-04T00:00:00Z'),
@@ -108,5 +94,6 @@ export const bankDefinitions: { [contractName: string]: BankInfo } = {
   },
 };
 
+// TODO: uncomment on mainnet release
 // export default configurations[process.env.NODE_ENV];
 export default configurations.development;
