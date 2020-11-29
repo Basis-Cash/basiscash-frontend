211b4008a79a3e09071daaa391993f08af18c54c
summersmooth <winterhard1234@gmail.com>
2020-11-30 07:57:41 +0900

    feat: Add mainnet deployment

--- src/config.ts
+++ src/config.ts
@@ -25,18 +25,17 @@ const configurations: { [env: string]: Configuration } = {
     chainId: ChainId.MAINNET,
     etherscanUrl: 'https://etherscan.io',
     defaultProvider: 'wss://mainnet.infura.io/ws/v3/d3bcb3c54e8c49a7bd1a9b6773e65bbe',
-    // TODO: replace with real mainnet deployment
-    deployments: require('./basis-cash/deployments/deployments.ropsten.json'),
+    deployments: require('./basis-cash/deployments/deployments.mainnet.json'),
     externalTokens: {
       DAI: ['0x6B175474E89094C44Da98b954EedeAC495271d0F', 18],
-      YFI: ['0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e', 18],
+      yCRV: ['0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', 18],
       SUSD: ['0x57Ab1E02fEE23774580C119740129eAC7081e9D3', 18],
       USDC: ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6],
       USDT: ['0xdAC17F958D2ee523a2206206994597C13D831ec7', 6],
-      'BAC_DAI-UNI-LPv2': ['0x6B175474E89094C44Da98b954EedeAC495271d0F', 18],
-      'BAS_DAI-UNI-LPv2': ['0x6B175474E89094C44Da98b954EedeAC495271d0F', 18],
+      'BAC_DAI-UNI-LPv2': ['0xd4405F0704621DBe9d4dEA60E128E0C3b26bddbD', 18],
+      'BAS_DAI-UNI-LPv2': ['0x0379dA7a5895D13037B6937b109fA8607a659ADF', 18],
     },
-    baseLaunchDate: new Date('2020-11-30T00:00:00Z'),
+    baseLaunchDate: new Date('2020-11-29T00:00:00Z'),
     bondLaunchesAt: new Date('2020-12-04T00:00:00Z'),
     boardroomLaunchesAt: new Date('2020-12-04T00:00:00Z'),
   },
@@ -94,6 +93,4 @@ export const bankDefinitions: { [contractName: string]: BankInfo } = {
   },
 };
 
-// TODO: uncomment on mainnet release
-// export default configurations[process.env.NODE_ENV];
-export default configurations.development;
+export default configurations[process.env.NODE_ENV || 'production'];
