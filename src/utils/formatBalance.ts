2229cc60f75588a94a51db8cc933446c9e6c0d72
summersmooth <winterhard1234@gmail.com>
2020-12-05 02:08:35 +0900

    fix: Resolve a bug when fraction is given

--- src/utils/formatBalance.ts
+++ src/utils/formatBalance.ts
@@ -1,10 +1,14 @@
 import { BigNumber } from 'ethers';
 
-export const getDisplayBalance = (balance: BigNumber, decimals = 18) => {
-  const number = balance.div(BigNumber.from(10).pow(decimals - 3)).toNumber();
-  return (number / 10 ** 3).toFixed(3);
-}
+export const getDisplayBalance = (balance: BigNumber, decimals = 18, fractionDigits = 3) => {
+  const number = getBalance(balance, decimals - fractionDigits);
+  return (number / 10 ** fractionDigits).toFixed(fractionDigits);
+};
 
 export const getFullDisplayBalance = (balance: BigNumber, decimals = 18) => {
   return getDisplayBalance(balance, decimals);
+};
+
+export function getBalance(balance: BigNumber, decimals = 18) : number {
+  return balance.div(BigNumber.from(10).pow(decimals)).toNumber();
 }
