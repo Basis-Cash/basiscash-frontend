ca84c5b745cdf6b568920936a0d32ffcbb4b0572
ByeongSu Hong <frostornge@gmail.com>
2020-12-06 00:47:29 +0900

    chore: remove comma

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -47,7 +47,7 @@ const Home: React.FC = () => {
         <Notice>
           Boardroom Seigniorage starts at <b>Dec 09 (Wed) 12:00am UTC</b>.
           For those who have already deposited Basis Shares into the Boardroom,&nbsp;
-         <b>we recommend that you withdraw your tokens and deposit them into the new boardroom contract.</b>.
+         <b>we recommend that you withdraw your tokens and deposit them into the new boardroom contract</b>.
         </Notice>
       </StyledNoticeContainer>
       <Spacer size="md" />
