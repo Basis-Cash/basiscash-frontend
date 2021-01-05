887c5ff9029b000849249a1f2805014ad9c8e646
powerdev0510 <sun.han510@gmail.com>
2021-01-05 18:23:45 +0800

    update

--- src/components/TopBar/components/AccountModal.tsx
+++ src/components/TopBar/components/AccountModal.tsx
@@ -13,13 +13,13 @@ import TokenSymbol from '../../TokenSymbol';
 const AccountModal: React.FC<ModalProps> = ({ onDismiss }) => {
   const basisCash = useBasisCash();
 
-  const bacBalance = useTokenBalance(basisCash.BAC);
+  const bacBalance = useTokenBalance(basisCash.JAM);
   const displayBacBalance = useMemo(() => getDisplayBalance(bacBalance), [bacBalance]);
 
-  const basBalance = useTokenBalance(basisCash.BAS);
+  const basBalance = useTokenBalance(basisCash.JAZZ);
   const displayBasBalance = useMemo(() => getDisplayBalance(basBalance), [basBalance]);
 
-  const babBalance = useTokenBalance(basisCash.BAB);
+  const babBalance = useTokenBalance(basisCash.JAB);
   const displayBabBalance = useMemo(() => getDisplayBalance(babBalance), [babBalance]);
 
   return (
