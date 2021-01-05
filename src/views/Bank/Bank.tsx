99b6480a5ed47788b7663c377e86f47848a0f4cd
powerdev0510 <sun.han510@gmail.com>
2021-01-05 18:33:42 +0800

    update

--- src/views/Bank/Bank.tsx
+++ src/views/Bank/Bank.tsx
@@ -58,11 +58,11 @@ const Bank: React.FC = () => {
 const LPTokenHelpText: React.FC<{ bank: BankEntity }> = ({ bank }) => {
   let pairName: string;
   let uniswapUrl: string;
-  if (bank.depositTokenName.includes('BAC')) {
-    pairName = 'BAC-DAI pair';
+  if (bank.depositTokenName.includes('JAM')) {
+    pairName = 'JAM-DAI pair';
     uniswapUrl = 'https://app.uniswap.org/#/add/0x3449FC1Cd036255BA1EB19d65fF4BA2b8903A69a/0x6B175474E89094C44Da98b954EedeAC495271d0F';
   } else {
-    pairName = 'BAS-DAI pair';
+    pairName = 'JAZZ-DAI pair';
     uniswapUrl = 'https://app.uniswap.org/#/add/0xa7ED29B253D8B4E3109ce07c80fc570f81B63696/0x6B175474E89094C44Da98b954EedeAC495271d0F';
   }
   return (
