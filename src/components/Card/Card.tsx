761358621fa6a8e48fd26cc5c678e03854afc566
s <stanley@mith.cash>
2021-01-17 21:00:00 +0800

    Change Card style

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,10 +8,8 @@ const Card: React.FC = ({ children }) => (
 )
 
 const StyledCard = styled.div`
-  background-color: ${props => props.theme.color.grey[800]};
-  border: 1px solid ${props => props.theme.color.grey[900]};
-  border-radius: 12px;
-  box-shadow: inset 1px 1px 0px ${props => props.theme.color.grey[700]};
+  background-color: ${props => props.theme.color.oblack};
+  border-radius: 20px;
   display: flex;
   flex: 1;
   flex-direction: column;
